e0b97cf6366151e0d75e423e97d31b73a475c196
habertrogena <habertorogy12@gmail.com>
2023-04-08 17:56:14 +0300

    added functionality to the email

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # product landing page
+## 
 
 
---
+++ js/main.js
@@ -0,0 +1,9 @@
+// let btn = document.getElementById('submit').onclick = function js(){
+//    console.log('button clicked')
+// }
+
+ function js(){
+    let btn = document.getElementById('submit');
+    alert('Email recieved succefully')
+ }
+ 
